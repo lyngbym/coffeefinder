d2c90ba3c3208d7b739e760944671e7308c0edda
Mason Lyngby <mason.lyngby@me.com>
2015-09-17 09:15:36 -0500

    Initial Commit

---
+++ coffeefinder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  coffeefinder
+//
+//  Created by Mason Lyngby on 9/17/15.
+//  Copyright © 2015 Mason Lyngby. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
